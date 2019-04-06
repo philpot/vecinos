f8901fc82d9c8f2d08f733c36cec1c722965ca21
Andrew Philpot <andrew.philpot@aofl.com>
2019-04-06 11:00:43 -0700

    second version

--- src/account_details.py
+++ src/account_details.py
@@ -9,19 +9,19 @@ config = MailChimpConfig()
 endpoint = config.api_root
 
 response = requests.get(endpoint, auth=('apikey', config.apikey))
-print response.url
+print(response.url)
 
 try:
   response.raise_for_status()
   response_json = response.json()
 except requests.exceptions.HTTPError as err:
-  print 'Error: %s' % err
+  print('Error: %s' % err)
 except ValueError:
-  print "Cannot decode JSON, got %s" % response.text
+  print("Cannot decode JSON, got %s" % response.text)
 
-print "Headers:"
+print("Headers:")
 for header in response.headers:
-    print '\t'.join(['',header.ljust(20), response.headers[header]])
+    print('\t'.join(['',header.ljust(20), response.headers[header]]))
 
-print "\nJSON:"
-print json.dumps(response_json, indent=4)
+print("\nJSON:")
+print(json.dumps(response_json, indent=4))
