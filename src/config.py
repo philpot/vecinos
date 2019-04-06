459ec0b9ed5f12b1036ef94c5fe09144dda35b1a
Andrew Philpot <andrew.philpot@aofl.com>
2019-04-06 10:57:37 -0700

    second version

--- src/config.py
+++ src/config.py
@@ -4,18 +4,18 @@ import sys
 
 class MailChimpConfig:
     def __init__(self):
-        if os.path.isfile('../APIKEY') == False:
-            print "Please enter your API Key into the APIKEY file as mentioned in README.md"
+        if os.path.isfile('../vault/APIKEY') == False:
+            print("Please enter your API Key into the APIKEY file as mentioned in README.md")
             sys.exit()
 
-        f = open('../APIKEY', 'r+')
+        f = open('../vault/APIKEY', 'r+')
         apikey = f.read().strip()
         f.close()
 
         parts = apikey.split('-')
         if len(parts) != 2:
-            print "This doesn't look like an API Key: " + apikey
-            print "The API Key should have both a key and a server name, separated by a dash, like this: abcdefg8abcdefg6abcdefg4-us1"
+            print("This doesn't look like an API Key: " + apikey)
+            print("The API Key should have both a key and a server name, separated by a dash, like this: abcdefg8abcdefg6abcdefg4-us1")
             sys.exit()
 
         self.apikey = apikey
